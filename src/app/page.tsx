360772b5cfd6dfe0da89ce712184908e1368a1f1
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-01-17 22:39:12 +0100

    Add main layout + README

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+	return (
+		<div className="min-w-screen min-h-screen">
+			<main className="flex flex-col gap-4 sm:p-4 md:p-16 lg:p-16">
+				<div>
+					<h1 className="text-7xl font-extrabold">Shoverglade</h1>
+					<p>
+						Reserve shower spots during Hack Clubs Overglade event!
+					</p>
+				</div>
+			</main>
+		</div>
+	);
+}
