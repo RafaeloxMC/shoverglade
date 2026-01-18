000c537287533c85a8361e06a3350a4f9ffb2645
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-01-18 09:16:13 +0100

    Add example dash page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+function DashboardPage() {
+	return (
+		<div className="min-w-screen min-h-screen bg-black">
+			<main className="flex flex-col p-4 gap-4">
+				<h1 className="text-4xl font-extrabold">Dashboard</h1>
+				<div>
+					<h2 className="text-2xl font-extrabold">Shower Schedule</h2>
+				</div>
+			</main>
+		</div>
+	);
+}
+
+export default DashboardPage;
