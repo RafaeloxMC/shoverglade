cbae374ee687efad54102f3cd157e00e977bfc15
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-01-19 15:27:55 +0100

    Fix cancellation of booking

--- src/app/api/v1/slots/cancel/route.ts
+++ src/app/api/v1/slots/cancel/route.ts
@@ -3,6 +3,7 @@ import { connectDB } from "@/database/db";
 import Slot from "@/database/schemas/Slot";
 import { cookies } from "next/headers";
 import { verify, JwtPayload } from "jsonwebtoken";
+import { Types } from "mongoose";
 
 async function getCurrentUserId() {
 	const cookieStore = await cookies();
@@ -39,7 +40,7 @@ export async function POST(request: Request) {
 
 		await connectDB();
 
-		await Slot.deleteOne(slotId);
+		await Slot.deleteOne(new Types.ObjectId(slotId));
 		return NextResponse.json({ success: true });
 	} catch (e) {
 		console.error("Cancel slot error:", e);
